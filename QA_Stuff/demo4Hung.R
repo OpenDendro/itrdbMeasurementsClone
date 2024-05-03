9b4ca12215fe484b97e5870c73d94a7701c6bed6
AndyBunn <bunna@wwu.edu>
2024-05-03 15:23:12 -0700

    A demo script for Hung edited

--- QA_Stuff/demo4Hung.R
+++ QA_Stuff/demo4Hung.R
@@ -3,4 +3,12 @@ library(dplR)
 source("QA_Stuff/read_tucson2.R")
 studies2check <- read.csv("QA_Stuff/studies_that_failed.csv")
 # grab a problem child
+<<<<<<< HEAD
 foo <- read.tucson2(studies2check$rwlfilename[1])
+=======
+# OG read.tucson works
+foo <- read.tucson(studies2check$rwlfilename[1])
+# new function doesn't
+foo <- read.tucson2(studies2check$rwlfilename[1])
+
+>>>>>>> 68aa4eeb (A demo script for Hung edited)
